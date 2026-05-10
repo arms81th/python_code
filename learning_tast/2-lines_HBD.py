55c10f80873201eb115acdcb7d317e8b70a20eb1
arms_thbo <takdanai8143@gmail.com>
2026-05-10 16:48:25 +0700

    Add birthday greeting script

---
+++ learning_tast/2-lines_HBD.py
@@ -0,0 +1,5 @@
+name,new_year,year=input("Please enter your name:"),int(input("Please enter Current year:")),int(input("Please enter your birth year:"))
+#new_year=int(input("Please enter Current year:")) กรอกปีปัจจุบัน
+#year=int(input("Please enter your birth year:")) กรอกปีเกิด
+
+print(f"You name is:\n= {name}\nYou birthday:\n= {year} year\nThis year you are old:\n= {new_year-year} year\n")
