83444e54c4bd3e6b649a0db5939bac511d03cabb
arms81th <takdanai8143@gmail.com>
2026-05-10 16:01:38 +0700

    Update If_eles_grad.py

--- learning_tast/If_eles_grad.py
+++ learning_tast/If_eles_grad.py
@@ -1,1 +1,17 @@
+#กำนดตัว input ต้องการป้อน
+score=int(input("Please enter your score: "))
+print("Your score is =",score)
 
+
+#เงื่อนไขการรทำงาน
+if score >= 80:
+    print("Your grade is A")
+elif score >= 70:
+    print("Your grade is B")
+elif score >= 60:
+    print("Your grade is C")
+elif score >= 50:
+    print("Your grade is D")
+
+else:
+    print("Your grade is F")
